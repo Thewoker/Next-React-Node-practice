0e2ee033cb9b88d670950308ae944035cc314575
Leonel Fernando Jara Ravarotto <lfjara@atp.local>
2023-11-24 12:47:09 -0300

    Update front cart detail

---
+++ taurus-shop/components/ShoppingProductCardDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShoppingProductCardDetail() {
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
--- taurus-shop/components/ui/ShoppingCart.jsx
+++ taurus-shop/components/ui/ShoppingCart.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ShoppingProductDetail from './../ShoppingProductDetail'
-import ShoppingCardDetail from './../ShoppingCardDetail'
+import React from "react";
+import ShoppingProductDetail from "./../ShoppingProductDetail";
+import ShoppingCardDetail from "./../ShoppingCardDetail";
 
 export default function ShoppingCart() {
   return (
-    <main className='main-container grid grid-cols-2 px-3 justify-items-center min-h-[90vh]'>
-        <div className='product-conteiner justify-items-center'>
-            <ShoppingProductDetail/>
+    <main className="main-container px-3 min-h-[90vh]">
+      <div className="grid grid-cols-3 justify-items-center px-3">
+        <div className="product-conteiner justify-items-center col-span-2">
+          <h2>Product</h2>
+          <div>
+            <ShoppingProductDetail />
+          </div>
         </div>
-        <div className='card-detail-conteiner'>
-            <ShoppingCardDetail/>
+        <div className="card-detail-conteiner col-span-1 row-span-1">
+          <h2>Product</h2>
+          <div>
+            <ShoppingCardDetail />
+          </div>
         </div>
+      </div>
     </main>
-  )
+  );
 }
